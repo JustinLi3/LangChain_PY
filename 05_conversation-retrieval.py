70e90118e4a606da049991a2322e18d6e8cec614
JustinLi3 <Triamethyest@gmail.com>
2024-06-21 16:02:32 -0400

    Testing

--- 05_conversation-retrieval.py
+++ 05_conversation-retrieval.py
@@ -1,45 +1,27 @@
-import os 
-from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder 
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_community.document_loaders import WebBaseLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
-from langchain_community.vectorstores.faiss import FAISS
-from langchain.chains import create_retrieval_chain  
-from langchain_core.messages import HumanMessage, AIMessage  
-from langchain.chains.history_aware_retriever import create_history_aware_retriever
 from typing import List, Union, Generator, Iterator
-import subprocess 
+from pydantic import BaseModel
+from schemas import OpenAIChatMessage
+import requests
+import os
 
 
+class Pipeline:
+    class Valves(BaseModel):
+        pass
 
-class Pipeline: 
     def __init__(self):
         # Optionally, you can set the id and name of the pipeline.
         # Best practice is to not specify the id so that it can be automatically inferred from the filename, so that users can install multiple versions of the same pipeline.
         # The identifier must be unique across all pipelines.
         # The identifier must be an alphanumeric string that can include underscores or hyphens. It cannot contain spaces, special characters, slashes, or backslashes.
-        # self.id = "python_code_pipeline"
-        self.name = "Python Code Pipeline" 
-        self.chatHistory = [  
-        #this is the roleplaying thing, kind of gives a background and placeholder
-        # HumanMessage(content = 'Hello'),
-        # AIMessage(content='Hello, how can I assist you?'), 
-        # HumanMessage(content = 'My name is Justin')
-        ]   
-        self.docs = None 
-        self.chain = None 
-        self.vectorStore = None
-        pass
+        # self.id = "wiki_pipeline"
+        self.name = "Wikipedia Pipeline"
+
+        # Initialize rate limits
+        self.valves = self.Valves(**{"OPENAI_API_KEY": os.getenv("OPENAI_API_KEY", "")})
 
     async def on_startup(self):
-        # This function is called when the server is started. 
-        self.docs = get_docs()
-        self.vectorStore = create_vector_store(self.docs)
-        self.chain = create_chain(self.vectorStore)    
-    
-        print("Assistant: Hello, how are you? Please feel free to ask me anything and type 'exit' to leave.")
+        # This function is called when the server is started.
         print(f"on_startup:{__name__}")
         pass
 
@@ -48,110 +30,40 @@ class Pipeline:
         print(f"on_shutdown:{__name__}")
         pass
 
-    def execute_python_code(self, code):
-        try:
-            result = subprocess.run(
-                ["python", "-c", code], capture_output=True, text=True, check=True
-            )
-            stdout = result.stdout.strip()
-            return stdout, result.returncode
-        except subprocess.CalledProcessError as e:
-            return e.output.strip(), e.returncode
-
     def pipe(
         self, user_message: str, model_id: str, messages: List[dict], body: dict
     ) -> Union[str, Generator, Iterator]:
         # This is where you can add your custom pipelines like RAG.
         print(f"pipe:{__name__}")
 
-        print(messages)
-        print(user_message)  
-        response = processQuestion(self.chain,user_message, self.chatHistory) 
-        self.chatHistory.append(HumanMessage(content = user_message))
-        self.chatHistory.append(AIMessage(content= response)) 
-        print("Assistant:", response)
-
         if body.get("title", False):
             print("Title Generation")
-            return "Python Code Pipeline"
+            return "Wikipedia Pipeline"
         else:
-            stdout, return_code = self.execute_python_code(user_message)
-            return stdout 
-        
-        
-def get_docs(): 
-    loader = WebBaseLoader('https://python.langchain.com/docs/expression_language/') 
-    docs = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=400,
-        chunk_overlap=20
-    )
-    splitDocs = text_splitter.split_documents(docs) 
-    print("DOCS RAN")
+            titles = []
+            for query in [user_message]:
+                query = query.replace(" ", "_")
 
-    return splitDocs
+                r = requests.get(
+                    f"https://en.wikipedia.org/w/api.php?action=opensearch&search={query}&limit=1&namespace=0&format=json"
+                )
 
-def create_vector_store(docs): 
-    embedding = OpenAIEmbeddings(
-        api_key = os.getenv("OPEN_API_KEY")
-        ) 
-    vectorStore = FAISS.from_documents(docs, embedding=embedding)
-    print("VS RAN")
-    return vectorStore
+                response = r.json()
+                titles = titles + response[1]
+                print(titles)
 
+            context = None
+            if len(titles) > 0:
+                r = requests.get(
+                    f"https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles={'|'.join(titles)}"
+                )
+                response = r.json()
+                # get extracts
+                pages = response["query"]["pages"]
+                for page in pages:
+                    if context == None:
+                        context = pages[page]["extract"] + "\n"
+                    else:
+                        context = context + pages[page]["extract"] + "\n"
 
-def create_chain(vectorStore):
-    model = ChatOpenAI(
-        api_key = os.getenv("OPEN_API_KEY"),
-        temperature=0.4,
-        model='gpt-3.5-turbo-1106'
-    )
-
-    prompt = ChatPromptTemplate.from_messages([
-        ("system", "Answer the user's questions based on the context: {context}"),  
-        #This is how you pass in a placeholder for background knowledge 
-        #Placeholder/Temp storage for messages, points to the list to use all the notes inside when responding 
-        MessagesPlaceholder(variable_name="chatHistory"),
-        ("human","{input}"),
-    ])
-    
-    document_chain = create_stuff_documents_chain(
-        llm=model,
-        prompt=prompt
-    )
-    #fetch top 3 vector chunks that closely relate to input, HOWEVER, we want to attach chat history too  
-    retriever = vectorStore.as_retriever(search_kwargs = {"k":3})  
-    retrieverPrompt = ChatPromptTemplate.from_messages([
-        MessagesPlaceholder(variable_name= "chatHistory"),
-        ("human", "{input}"),
-        ("human", "Given the above conversation, generate a search query to get information relevant to the conversation")
-    ])
-    historyAwareRetriever = create_history_aware_retriever( 
-        llm = model,  
-        retriever = retriever, 
-        prompt = retrieverPrompt,
-    )
-    retrieval_chain = create_retrieval_chain(historyAwareRetriever, document_chain)
-    print("RETRIEVAL CHAIN RAN")
-
-    return retrieval_chain
-
-def processQuestion(chain, question, chatHistory): 
-    response = chain.invoke({
-    "input": question,  
-    #Make sure that when processing the question that you include this for history
-    "chatHistory" : chatHistory
-    })
-    #response would, because of retrieval chain, return a dictionary of input, context, and answer
-    print("QUESTION")
-
-    return response["answer"]
-
-#Similar to public static void main..
-
-    
-
-
-
-
-
+            return context if context else "No information found"
