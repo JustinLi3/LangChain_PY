b7913e4f337114af2688df0d67f9a5634bdb44ba
JustinLi3 <Triamethyest@gmail.com>
2024-06-20 23:22:47 -0400

    Simple RAG with ChromaDB

--- rag/simplerag.py
+++ rag/simplerag.py
@@ -4,10 +4,10 @@
 from langchain_community.document_loaders import TextLoader 
 loader1 = TextLoader("rag/rag_pipeline.txt") 
 text_document = loader1.load() 
-print(text_document) 
+# print(text_document) 
 
-# import os
-# os.environ['OPENAI_API_KEY'] = os.getenv("OPEN_API_KEY") 
+import os
+os.environ['OPENAI_API_KEY'] = os.getenv("OPEN_API_KEY") 
 
 
 #Text based loader
@@ -15,11 +15,24 @@ from langchain_community.document_loaders import WebBaseLoader
 import bs4 
 loader2 = WebBaseLoader("https://www.californiastrawberries.com/can-strawberries-improve-heart-health/") 
 web_document = loader2.load()
-print(web_document)
+# print(web_document)
  
 #PDF loader
 from langchain_community.document_loaders import PyPDFLoader 
 loader3 = PyPDFLoader("rag/formadv.pdf")   
 pdf_document = loader3.load() 
-print(pdf_document)
+# print(pdf_document)
 
+from langchain.text_splitter import RecursiveCharacterTextSplitter 
+text_splitter = RecursiveCharacterTextSplitter(chunk_size = 150, chunk_overlap = 30) 
+chunks = text_splitter.split_documents(pdf_document) 
+
+#Vector store and embeddings
+from langchain_community.embeddings import OpenAIEmbeddings 
+from langchain_community.vectorstores import Chroma  
+#get first 20 chunks, using the embeddings to vectorize and store into vector store
+db = Chroma.from_documents(chunks[:20],OpenAIEmbeddings()) 
+
+query = input("Please enter a question: ")
+result = db.similarity_search(query) 
+print(result[0].page_content)
